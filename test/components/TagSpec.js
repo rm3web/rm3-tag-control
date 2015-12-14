8514ac6c1bf417c117b0c78674e2a06147745ec5
Ken Wronkiewicz <wh-github@wirewd.com>
2015-12-13 17:37:36 -0800

    adding interactive test.

--- test/components/TagSpec.js
+++ test/components/TagSpec.js
@@ -1,4 +1,5 @@
 import Tag from '../../src/components/tags/tag';
+import { findDOMNode } from 'react-dom';
 
 describe('Tag', () => {
   context('props.tag', () => {
@@ -8,6 +9,21 @@ describe('Tag', () => {
       $tag.find(Tag).length.should.equal(1);
 
       $tag.find(Tag)[0].props.tag.should.equal('pretty pony')
+    });
+
+    it('should allow for deletion', (cb) => {
+      function removeTagTest() {
+        cb();
+      }
+
+      let detachedComp = TestUtils.renderIntoDocument(
+        <Tag tag='pretty pony' removeTag={removeTagTest} />
+      )
+      let button = TestUtils.findRenderedDOMComponentWithTag(detachedComp, 'button')
+      let buttonNode = findDOMNode(button);
+
+      TestUtils.Simulate.click(buttonNode);
+
     });
   });
 });
