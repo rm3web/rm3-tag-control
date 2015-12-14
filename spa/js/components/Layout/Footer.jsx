e8f14b3f53878b50adf238c1b70c0356720c5362
Ken Wronkiewicz <wh-github@wirewd.com>
2015-12-13 17:42:14 -0800

    remove copyright notice.

--- spa/js/components/Layout/Footer.jsx
+++ spa/js/components/Layout/Footer.jsx
@@ -2,9 +2,6 @@ import { config } from '../../../../src';
 
 const Footer = () => (
   <footer className="container">
-    <center>
-      All copyrights reserved &copy; <b>{ config.author || '<package.json:author>' }</b> { (new Date()).getFullYear() }
-    </center>
     <p>&nbsp;</p>
   </footer>
 );
